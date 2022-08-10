ca6bfb6ea46b89cffd6b485fab6d07ce0b66b7fb
alx-b <>
2022-08-10 15:33:55 +0200

    Updated server file and added docker container files

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
-const application = express()
-
-application.use(express.json())
+import cors from 'cors'
+import CustomerRoutes from './src/routes/Customer.routes.js'
 
 dotenv.config()
-const { PORT, ENVIRONMENT } = process.env
+const { PORT, ENVIRONMENT, MONGO_DB_URI } = process.env
 
-try {
-  application.listen(PORT, () => {console.log(`SERVER RUNNING ON PORT ${PORT}`)})
-} catch (error) {
-  console.error(`Could not listen on port ${PORT}`)
+const application = express()
+application.use(express.json())
+application.use(cors({credential: true}))
+
+CustomerRoutes.routes(application)
+
+const connectToPort = async () => {
+  try {
+    application.listen(PORT, () => {console.log(`SERVER RUNNING ON PORT ${PORT}`)})
+  } catch (error) {
+    console.error(`Could not listen on port ${PORT}`)
+  }
 }
+
+const connectToDB = async () => {
+  const DB_URI = ENVIRONMENT === 'DEVELOPMENT' ? MONGO_DB_URI : "NONE"
+  try {
+    await mongoose.connect(
+      DB_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    console.log('Successfully connected to the database.')
+  } catch (error) {
+    console.error(`Error while trying to connect to the database: ${error}`)
+    process.exit()
+  }
+}
+
+connectToPort()
+connectToDB()
